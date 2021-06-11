0514cd02f4830fb827c14f15ea46ff7793030afd
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-06-11 18:07:32 +0530

    Errors handling system added

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-// import { CustomError } from '../errors/custom-error';
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -7,11 +7,11 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  // if (err instanceof CustomError) {
-  //   return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  // }
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
   res.status(400).send({
-    errors: [{ message: "Something went wrong" }],
+    errors: [{ message: 'Something went wrong' }]
   });
 };
