3df63e602aa93613a746812d2cba78237e601bff
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-07-05 01:26:22 +0530

    Seperate build-client added

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   // console.log(currentUser);
@@ -8,28 +8,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === "undefined") {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: {
-          Host: "ticketing.dev",
-        },
-      }
-    );
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get("/api/users/currentuser");
-
-    return data;
-  }
-  return {};
+  return data;
 };
 
 export default LandingPage;
