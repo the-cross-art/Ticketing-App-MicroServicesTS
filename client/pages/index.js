3d283bc785547604a278821c20ac1d587196c2fc
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-07-17 01:59:45 +0530

    gloabal header declared

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,15 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser');
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async context => {
+  console.log('LANDING PAGE!');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
 
