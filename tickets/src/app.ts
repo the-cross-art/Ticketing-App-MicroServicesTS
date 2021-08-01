4fc930206a1c811f1c2097589f9014b14f09a237
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-08-02 00:37:26 +0530

    New test cases added

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,9 @@ import {
   currentUser,
 } from "@modelclubauth/common";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -21,6 +24,9 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
