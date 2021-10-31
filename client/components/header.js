6e61ef4e20a86df7f3e56cd59041647353cf46ef
the-cross-art <imrannaz326@gmail.com>
2021-11-01 00:54:12 +0530

    LINUX support for microservices added

--- client/components/header.js
+++ client/components/header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default ({ currentUser }) => {
+const header = ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
@@ -29,3 +29,5 @@ export default ({ currentUser }) => {
     </nav>
   );
 };
+
+export default header;
