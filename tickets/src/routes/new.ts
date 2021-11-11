e935cf8282d1353a7fc39452276780a176c1993b
the-cross-art <imrannaz326@gmail.com>
2021-11-12 01:20:13 +0530

    Ticket NATS integeration

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,18 +1,20 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import { requireAuth, validateRequest } from "@modelclubauth/common";
-import { Ticket } from "../models/ticket";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@modelclubauth/common';
+import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.post(
-  "/api/tickets",
+  '/api/tickets',
   requireAuth,
   [
-    body("title").not().isEmpty().withMessage("Title is required"),
-    body("price")
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
       .isFloat({ gt: 0 })
-      .withMessage("Price must be greater than 0"),
+      .withMessage('Price must be greater than 0'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -24,6 +26,12 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
