c3e2c7361374b3da01e8b5594f70bde9551fffec
Imran Nazir <63959831+the-cross-art@users.noreply.github.com>
2021-12-11 20:03:44 +0530

    Expiration service NATS connection Redis server

--- expiration/src/nats-wrapper.ts
+++ expiration/src/nats-wrapper.ts
@@ -14,7 +14,7 @@ class NatsWrapper {
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
 
-    return new Promise((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
       this.client.on('connect', () => {
         console.log('Connected to NATS');
         resolve();
